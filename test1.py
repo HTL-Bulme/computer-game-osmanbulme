7dd20dc1085922b29cb3d5b3f583a323cb4ae070
osman cicekdag <osman.cicekdag@bulme.at>
2024-02-29 17:26:30 +0100

    NEW

---
+++ test1.py
@@ -0,0 +1,26 @@
+width = 20
+height = 6/2
+
+text = "das ist 1"
+#print (text)
+
+wert = 2*2
+#print(width * height, text) mit , komma andere variable ausgeben
+
+width * height
+
+#print(415* 'un' + 'ium')
+
+text = ('Put several strings within parentheses '
+
+        "to have them joined together.")
+
+#print(text)
+
+word = 'Python'
+
+#print(word[0])
+
+letters = balls
+
+len(letters)
